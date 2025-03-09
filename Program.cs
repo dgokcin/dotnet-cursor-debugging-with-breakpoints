5579852339c7ae459043c562acb16cf4733e9c3a
deniz gökçin <33603535+dgokcin@users.noreply.github.com>
2025-03-09 01:03:22 +0100

    docs: add coffee donation link to console message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿string message = "Please leave a star if you managed to stop at this breakpoint!";
+﻿string message = "Please leave a star if you managed to stop at this breakpoint!\n" +
+                "If you're feeling extra generous, you can also buy me a coffee at https://buymeacoffee.com/dgokcin - it would mean a lot!";
 
 Console.WriteLine(message);
